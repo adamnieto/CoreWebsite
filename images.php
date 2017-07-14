2cc67946c4ffa1852e404ecd97432091d3c32d7b
root <jlevin38@binghamton.edu>
2017-07-14 22:04:58 +0000

    fixed annoying modal glitch by setting a default image source

--- images.php
+++ images.php
@@ -1,31 +1,40 @@
-var directories = new Map();
 var index = 0;
 
+var jq = document.createElement('jq');
+jq.src="https://code.jquery.com/jquery-1.10.2.min.js";
+jq.type='text/javascript';
+document.getElementsByTagName('head')[0].appendChild(jq);
+
 <?php
-	chdir("assets/images");
-	$dirs = array_filter(glob('*'), 'is_dir');
-	echo "var arr; \n";
-	foreach($dirs as $dir){
-		echo "arr = new Array(); \n";
-		$files = scandir($dir);
-		foreach($files as $file){
-			if($file == "." || $file == ".."){
-				continue;
-			}
-			echo "arr.push(\"" . $file . "\"); \n";
-		}
-		echo "directories.set(\"" . $dir ."\", arr); \n";
-	}
+
+    echo "var directories = new Map();";
+    chdir("assets/images");
+    $dirs = array_filter(glob('*'), 'is_dir');
+    echo "var arr; \n";
+    foreach($dirs as $dir){
+        echo "arr = new Array(); \n";
+        $files = scandir($dir);
+        foreach($files as $file){
+            if($file == "." || $file == ".."){
+                continue;
+            }
+            echo "arr.push(\"" . $file . "\"); \n";
+        }
+        echo "directories.set(\"" . $dir ."\", arr); \n";
+    }
 ?>
 
 function refreshImage(folder, noIndexReset){
 	if(!noIndexReset)
 		index = 0;
 	console.log(index);
-	document.getElementById("next").setAttribute("onclick","next(\"" + folder +"\")");
-	document.getElementById("prev").setAttribute("onclick","prev(\"" + folder +"\")");
-	document.getElementById("picture").src = 
-		"assets/images/" + folder + "/" + directories.get(folder)[index];
+    document.getElementById("next").setAttribute("onclick","next(\"" + folder +"\")");
+    document.getElementById("prev").setAttribute("onclick","prev(\"" + folder +"\")");
+    document.getElementById("picture").src = 
+        "assets/images/" + folder + "/" + directories.get(folder)[index]; 
+    $('.ui.modal').modal('show');
+    
+
 
 }
 
